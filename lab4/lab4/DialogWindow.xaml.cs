0432f4f17b98c56ee59dd20b5b8e949dfe95f5ae
Alexander Voronkov <pubgplayer29112004@gmail.com>
2023-10-26 02:49:59 +0300

    lab4 done

---
+++ lab4/lab4/DialogWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab4
+{
+    /// <summary>
+    /// Interaction logic for DialogWindow.xaml
+    /// </summary>
+    public partial class DialogWindow : Window
+    {
+        public string Message { get; set; }
+        public DialogWindow(string[] radio)
+        {
+            InitializeComponent();
+            if(radio != null && radio.Length > 0)
+            {
+                foreach (var item in radio)
+                {
+                    RadioContainer.Children.Add(new RadioButton() { Content = item, VerticalContentAlignment = VerticalAlignment.Center, GroupName = "radiobtns" });
+                }
+                (RadioContainer.Children[0] as RadioButton)!.IsChecked = true;
+            }      
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Message += RadioContainer.Children
+                .Cast<RadioButton>()
+                .Where(x => x.IsChecked == true)
+                .ElementAt(0).Content.ToString();
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void MessageText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Message = (sender as TextBox)!.Text;
+        }
+    }
+}
---
+++ lab4/lab4/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogWindow dialogWindow = new DialogWindow((sender as Button).Content.ToString().Split(','));
+            if (dialogWindow.ShowDialog() == true)
+            {
+                DialogResult.Text = dialogWindow.Message;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Cpp-files|*.cpp|C-files|*.c|Header-files|*.h";
+            var res = ofd.ShowDialog();
+            if(res == true)
+            {
+                DialogResult.Text = System.IO.Path.GetExtension(ofd.FileName);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Which option");
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("No dialog");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
